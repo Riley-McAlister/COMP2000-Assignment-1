f8f20f872b7d32b10bbaf6bdcf975adabcf1c329
Riley <riley.mcalister@students.mq.edu.au>
2026-08-17 16:47:26 +1000

    Add files via upload

--- Animal.java
+++ Animal.java
@@ -1,5 +1,37 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class Animal{
+
+    public int health;
+    public int topSpeed;
+    public int age;
+    public int breedingRate;
+    public int energy;
+
+    //eat doubles energy, capped at 100, if energy below 0 animal dies.
+    void eat(){
+        energy = Math.clamp(energy*2, 0, 100);
+    }
+    //sleep doubles current health, capped at 100, if health below 0, animal dies.
+    void Sleep() {
+        health = Math.clamp(health*2, 0, 100);
+    }
+
+}
+
+class Dingo extends Animal{
+    //distance at which predator detects prey
+    int scentRange;
+
+    //object position change, rate of movement capped at topSpeed;
+    void hop(){
     }
 }
+
+class Kangaroo extends Animal{
+    //distance at which prey detects a predator
+    int predatorDetectionRange;
+
+    //object position change, rate of movement capped at topSpeed;
+    void run(){
+    }
+}
+
